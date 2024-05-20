b499798ea901de7cdac07627e5207b30ffd0a32a
Mortal <clantatarsky@gmail.com>
2024-05-21 01:54:49 +0400

    Homework 4.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,23 +7,29 @@ import ru.pflb.mq.dummy.interfaces.Destination;
 import ru.pflb.mq.dummy.interfaces.Producer;
 import ru.pflb.mq.dummy.interfaces.Session;
 
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws DummyException {
+    public static void main(String[] args) throws DummyException, IOException {
         Connection connection = null;
         Session session = null;
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-        String[] array = {"Четыре", "Пять", "Шесть"};
+        String filePath = args[0];
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        List<String> list = reader.lines().toList();
+        reader.close();
         try {
             connection = new ConnectionImpl();
             session = connection.createSession(true);
             Destination destination = session.createDestination("myQueue");
             Producer producer = session.createProducer(destination);
-            for (int i = 0; i < array.length; i++) {
-                producer.send(array[i]);
-                Thread.sleep(2000);
-            }
+            do {
+                for (String s : list) {
+                    producer.send(s);
+                    Thread.sleep(2000);
+                }
+            } while (!new Scanner(System.in).nextLine().equals("stop"));
         } catch (DummyException exception) {
             throw new DummyException();
         } catch (InterruptedException e) {
@@ -33,7 +39,6 @@ public class Main {
                 session.close();
             if (connection != null)
                 connection.close();
-
         }
     }
 }
